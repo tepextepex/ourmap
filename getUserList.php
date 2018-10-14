046c23136c35e076da8bcaed625c6a1bbbc9b276
tepextepex <tepextepex@gmail.com>
2018-10-14 16:22:56 +0300

    ajax-called scripts to populate the map with user stats

---
+++ getUserList.php
@@ -0,0 +1,10 @@
+<?php
+	include "auth/const.php";
+	$con = new mysqli(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME);
+	$result = $con->query("SELECT user_login FROM users WHERE user_login <> ''");
+	$user_list = array();
+	while ($row = mysqli_fetch_assoc($result)) {
+		$user_list[] = $row;
+	}
+	$con->close();
+	echo json_encode($user_list);
---
+++ getVisitedCountries.php
@@ -0,0 +1,22 @@
+<?php
+	include "auth/const.php";
+	include "auth/auth.php";
+	$con = new mysqli(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME);
+	$username = auth_check();
+	if ($username) {//if user is logged in
+		if (!empty($_GET['who']) && $_GET['who'] == $username) {
+			$query = "SELECT * FROM world WHERE who = '$username'";
+		} else {
+			$who = $_GET['who'];
+			$query = "SELECT * FROM world WHERE who = '$who'";
+		}		
+	} else {
+		$query = "SELECT * FROM world WHERE who = 'Tepextepex'";
+	};
+	$result = $con->query($query);
+	$rows = array();
+	while ($row = mysqli_fetch_assoc($result)) {
+		$rows = $row;
+	}
+	$con->close();
+	echo json_encode($rows);
